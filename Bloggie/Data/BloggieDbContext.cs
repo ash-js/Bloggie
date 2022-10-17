e0cefcf1edccfb58039daedbe42624570c963552
ash89 <33664687+ashor89@users.noreply.github.com>
2022-10-17 22:51:42 +1100

    Add DbContext

--- Bloggie/Data/BloggieDbContext.cs
+++ Bloggie/Data/BloggieDbContext.cs
@@ -1,6 +1,16 @@
-﻿namespace Bloggie.Data
+﻿using Bloggie.Models.Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.Data
 {
-    public class BloggieDbContext
+    public class BloggieDbContext : DbContext
     {
+        public BloggieDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
     }
 }
